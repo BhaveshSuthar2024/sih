6dc3afe5e736aa082adafdb743901383656ad9be
BhaveshSuthar2024 <bsuthar2024@gmail.com>
2024-09-20 16:29:22 +0530

    Add files via upload

---
+++ sih web/script.js
@@ -0,0 +1,16 @@
+document.getElementById("zone").addEventListener("click", function() {
+    if (document.getElementById("zone_el").style.display == "none") {
+        document.getElementById("zone_el").style.display = "block";
+    }
+    else {
+        document.getElementById("zone_el").style.display = "none";
+    }
+});
+document.getElementById("Logo").addEventListener("click", function() {
+    if (document.getElementById("Sidebar").style.display == "none") {
+        document.getElementById("Sidebar").style.display = "block";
+    }
+    else {
+        document.getElementById("Sidebar").style.display = "none";
+    }
+});
